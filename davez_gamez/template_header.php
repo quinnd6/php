f74954c5416a4dd28306e3a646a1a9a8e435c97c
quinnd6 <quinnd6@msn.com>
2015-09-16 22:09:36 +0100

    made a change to template_header.php

--- davez_gamez/template_header.php
+++ davez_gamez/template_header.php
@@ -1,7 +1,7 @@
 <div id="pageHeader">
                 <table width="100%" border="0" cellspacing="0" cellpadding="12"> 
                     <tr>
-                        <td width="32%"><a href="http://localhost/davez_gamez/storeadmin/"><img src="http://localhost/MyOnlineStore/style/logo.jpg" alt="Logo" width="252" height="36" border="0"/></a> </td>
+                        <td width="32%"><a href="http://localhost/davez_gamez/storeadmin/"><img src="http://localhost/davez_gamez/images/davezgamez.jpg" alt="Logo" width="252" height="36" border="0"/></a> </td>
                         
                     </tr>
                     <tr>
