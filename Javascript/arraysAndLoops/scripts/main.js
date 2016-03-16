77c62574f7235c598ed72f648b46020b960b8e4f
quinnd6 <quinnd6@msn.com>
2016-03-16 19:41:05 +0000

    Javascript code added

---
+++ Javascript/arraysAndLoops/scripts/main.js
@@ -0,0 +1,5 @@
+var myList = ['Apples', 'Oranges', 'Bananas']
+for (var i=0; i<myList.length; i++)
+{
+    alert('You have '+myList[i]+' in your basket');
+}
---
+++ Javascript/selectors/scripts/main.js
@@ -0,0 +1,14 @@
+//piece of code that does one or more actions
+function go(name, age)
+{
+   if(age < 20)
+   {
+       return name+'!';
+   }
+   else
+   {
+       return name;
+   }
+}
+
+alert(go('Will', 19))
