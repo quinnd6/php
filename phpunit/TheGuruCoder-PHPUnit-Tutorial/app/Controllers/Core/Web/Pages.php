045b748f06f14d71165fef24312bf9aafb782fef
quinnd6 <quinnd6@msn.com>
2016-03-30 19:04:05 +0100

    PHPUnit Example

---
+++ phpunit/TheGuruCoder-PHPUnit-Tutorial/app/Controllers/Core/Web/Pages.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Controllers\Core\Web;
+
+class Pages
+{
+    public function render()
+    {
+        return "Hello World";
+    }
+    
+    public function returnTrue()
+    {
+        return true;
+    }
+    
+    public function returnArray()
+    {
+        return ['Hello', 'World', 'This', 'Is', 'An', 'Array'];
+    }
+    
+}
+?>
---
+++ phpunit/TheGuruCoder-PHPUnit-Tutorial/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$pages = new \Controller\Core\Web\Pages();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TheGuruCoder PHPUnit Tutorial</title>
+</head>
+<body>
+    <h1><?php echo $pages->render(); ?></h1>
+    <pre><?php var_dump($pages->returnArray()); ?></pre>
+</body>
+</html>
